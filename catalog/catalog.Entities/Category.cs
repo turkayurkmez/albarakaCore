95f0948e57511768db94fcc4cf97552b76c6c7e0
Türkay Ürkmez <turkay@turkayurkmez.com>
2022-09-22 09:50:46 +0300

    Creating some entities to Entity Layer

---
+++ catalog/catalog.Entities/Category.cs
@@ -0,0 +1,11 @@
+﻿namespace catalog.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public ICollection<Product> Products { get; set; }
+
+    }
+}
---
+++ catalog/catalog.Entities/Product.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace catalog.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Ürün adı boş olamaz....")]
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string? Description { get; set; }
+        public double? Rating { get; set; }
+
+        public bool IsActive { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        public int StockCount { get; set; }
+
+        public Category Category { get; set; }
+
+
+    }
+}
